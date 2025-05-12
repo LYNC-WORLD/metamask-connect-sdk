a604ecf04173e4816d5da1ef1b4b8f260bfd1994
Bobby-Lync <bobby@lync.world>
2025-05-12 09:59:44 +0530

    add message signing example

--- examples/vite-app/src/lib/utils.ts
+++ examples/vite-app/src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs));
+};
+
+export const getSigningMessage = (account: string, chainId: string) => {
+  if (!account || !chainId) return '';
+
+  return `${
+    window.location.host
+  } wants you to sign in with your Ethereum account:\n${account}\n\nI accept the authentication request from LYNC World.\n\nURL: https://${
+    window.location.host
+  }\nChain ID: ${chainId}\nIssued At: ${new Date().toISOString()}`;
+};
